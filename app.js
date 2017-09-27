55547cffb42063d9a3c40412f296f5a8d03de2db
Lee Jun Yeong <leejunyeong@sgw-u502871l001.ad.seagate.com>
2017-09-27 10:49:17 +0800

    added gulp, handlebars and jade

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 var express = require('express');
 
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+
+var handlebars = require('express-handlebars');
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 app.listen(port, function (err) {
     console.log('running server on port ' + port);
 });
 
 app.get('/', function (req, res) {
-    res.send('Hello, World');
+    res.render('index', {title: 'Hello from render', list: ['a', 'b']});
 });
 
 app.get('/books', function (req, res) {
