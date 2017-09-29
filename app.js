cae9bdae42749b041400a0035915c76d0932d115
Lee Jun Yeong <leejunyeong@lees-mbp.woo.sing.seagate.com>
2017-09-29 19:13:38 +0800

    Added authentication and integrated 3rd party APIs

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
 
 var app = express();
 var port = process.env.PORT || 5000;
@@ -7,8 +11,15 @@ var nav = [
     { Link: '/Authors', Text: 'Authors' }];
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+require('./src/config/passport')(app);
+
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
@@ -19,6 +30,7 @@ app.get('/', function (req, res) {
 });
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 app.listen(port, function (err) {
     console.log('running server on port ' + port);
---
+++ src/services/goodreadsService.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var xml2js = require('xml2js');
+var parser = xml2js.Parser({ explicitArray: false });
+
+var goodreadsService = function () {
+    var getBookById = function (id, cb) {
+        var options = {
+            host: 'www.goodreads.com',
+            path: '/book/show/' + id + '?format=xml&key=0NLRVDb0UIOEpD4lcDjEQ',
+        };
+        var callback = function (response) {
+            var str = '';
+            response.on('data', function (chunk) {
+                str += chunk;
+            });
+            response.on('end', function () {
+                //console.log(str);
+                parser.parseString(str, function (err, results) {
+                    cb(null, results.GoodreadsResponse.book);
+                });
+            });
+        };
+        http.request(options, callback).end();
+    };
+    return {
+        getBookById: getBookById
+    };
+};
+
+module.exports = goodreadsService;
